59a225cec8a3cd21c57aabd493916b3331345b76
Rayyan <97803050+RayyanW786@users.noreply.github.com>
2024-04-18 13:10:00 +0100

    Create src.py
    init

---
+++ src.py
@@ -0,0 +1,29 @@
+from typing import List, Tuple
+
+
+def resolve_power(index: int, number: int) -> int:
+    return number ** index
+
+
+def resolve_disarium(user: str, numbers: List[int]) -> Tuple[int, List[str], bool]:
+    resolved_value = 0
+    mapper: List[str] = []
+    for idx, number in enumerate(numbers, start=1):
+        resolved_value += resolve_power(idx, number)
+        mapping.append(f"{number}**{idx}")
+    return resolved_value, mapping, str(resolved_value) == user_input
+
+
+while True:
+    user_input: str = input(
+        "enter your number: "
+    )
+    try:
+        valid_numbers: int = int(user_input)
+        valid_numbers: List[int] = [int(e) for e in str(valid_numbers)]
+    except ValueError:
+        print("Invalid Number")
+        continue
+    value, mapping, pred = resolve_disarium(user_input, valid_numbers)
+    print(f"Your number is {'NOT ' if not pred else ''}a disarium number"
+          f" {(' + '.join(mapping) + ' = ' + str(value)) if pred else ''}\n")
